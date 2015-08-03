1cfffaf6e1bacf126ad3b06089e9f78806e99405
Nalipp <nateplipp@gmail.com>
2015-08-03 09:35:50 +0900

    build country controller

---
+++ app/controllers/countries_controller.rb
@@ -0,0 +1,82 @@
+class CountriesController < ApplicationController
+  def home
+  end
+
+  def contact
+  end
+
+  def south_korea
+  end
+
+  def australia
+  end
+
+  def austria
+  end
+
+  def belgium
+  end
+
+  def canada
+  end
+
+  def chile
+  end
+
+  def czech_republic
+  end
+
+  def denmark
+  end
+
+  def france
+  end
+
+  def germany
+  end
+
+  def hungary
+  end
+
+  def ireland
+  end
+
+  def israel
+  end
+
+  def italy
+  end
+
+  def japan
+  end
+
+  def mexico
+  end
+
+  def the_netherlands
+  end
+
+  def new_zealand
+  end
+
+  def poland
+  end
+
+  def portugal
+  end
+
+  def romania
+  end
+
+  def sweden
+  end
+
+  def taiwan
+  end
+
+  def united_kingdom
+  end
+
+  def usa
+  end
+end
---
+++ test/controllers/countries_controller_test.rb
@@ -0,0 +1,139 @@
+require 'test_helper'
+
+class CountriesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+
+  test "should get south_korea" do
+    get :south_korea
+    assert_response :success
+  end
+
+  test "should get australia" do
+    get :australia
+    assert_response :success
+  end
+
+  test "should get austria" do
+    get :austria
+    assert_response :success
+  end
+
+  test "should get belgium" do
+    get :belgium
+    assert_response :success
+  end
+
+  test "should get canada" do
+    get :canada
+    assert_response :success
+  end
+
+  test "should get chile" do
+    get :chile
+    assert_response :success
+  end
+
+  test "should get czech_republic" do
+    get :czech_republic
+    assert_response :success
+  end
+
+  test "should get denmark" do
+    get :denmark
+    assert_response :success
+  end
+
+  test "should get france" do
+    get :france
+    assert_response :success
+  end
+
+  test "should get germany" do
+    get :germany
+    assert_response :success
+  end
+
+  test "should get hungary" do
+    get :hungary
+    assert_response :success
+  end
+
+  test "should get ireland" do
+    get :ireland
+    assert_response :success
+  end
+
+  test "should get israel" do
+    get :israel
+    assert_response :success
+  end
+
+  test "should get italy" do
+    get :italy
+    assert_response :success
+  end
+
+  test "should get japan" do
+    get :japan
+    assert_response :success
+  end
+
+  test "should get mexico" do
+    get :mexico
+    assert_response :success
+  end
+
+  test "should get the_netherlands" do
+    get :the_netherlands
+    assert_response :success
+  end
+
+  test "should get new_zealand" do
+    get :new_zealand
+    assert_response :success
+  end
+
+  test "should get poland" do
+    get :poland
+    assert_response :success
+  end
+
+  test "should get portugal" do
+    get :portugal
+    assert_response :success
+  end
+
+  test "should get romania" do
+    get :romania
+    assert_response :success
+  end
+
+  test "should get sweden" do
+    get :sweden
+    assert_response :success
+  end
+
+  test "should get taiwan" do
+    get :taiwan
+    assert_response :success
+  end
+
+  test "should get united_kingdom" do
+    get :united_kingdom
+    assert_response :success
+  end
+
+  test "should get usa" do
+    get :usa
+    assert_response :success
+  end
+
+end
